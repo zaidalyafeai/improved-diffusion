9aabf1045b7b66e8ee95a23c2cb71e4fbb1859fe
nostalgebraist <nostalgebraist@gmail.com>
2021-11-13 13:25:54 -0800

    work

--- improved_diffusion/unet.py
+++ improved_diffusion/unet.py
@@ -543,6 +543,7 @@ class UNetModel(nn.Module):
         :param y: an [N] Tensor of labels, if class-conditional.
         :return: an [N x C x ...] Tensor of outputs.
         """
+        print(f"forward: txt passed = {txt is not None}, model txt = {self.txt}")
         assert (y is not None) == (
             self.num_classes is not None
         ), "must specify y if and only if the model is class-conditional"
