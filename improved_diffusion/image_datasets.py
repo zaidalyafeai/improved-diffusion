0e5115d5d6242c7cc767ed04007024c0373c8ff8
nostalgebraist <nostalgebraist@gmail.com>
2021-11-13 12:31:12 -0800

    text loader

--- improved_diffusion/image_datasets.py
+++ improved_diffusion/image_datasets.py
@@ -6,7 +6,8 @@ from torch.utils.data import DataLoader, Dataset
 
 
 def load_data(
-    *, data_dir, batch_size, image_size, class_cond=False, deterministic=False
+    *, data_dir, batch_size, image_size, class_cond=False, deterministic=False,
+    txt=False
 ):
     """
     For a dataset, create a generator over (images, kwargs) pairs.
@@ -26,7 +27,9 @@ def load_data(
     """
     if not data_dir:
         raise ValueError("unspecified data directory")
-    all_files = _list_image_files_recursively(data_dir)
+    all_files, image_file_to_text_file = _list_image_files_recursively(data_dir, txt=txt)
+    print(f"found {len(all_files)} images, {len(image_file_to_text_file)} texts")
+
     classes = None
     if class_cond:
         # Assume classes are the first part of the filename,
@@ -38,6 +41,8 @@ def load_data(
         image_size,
         all_files,
         classes=classes,
+        image_file_to_text_file=image_file_to_text_file,
+        txt=txt,
         shard=MPI.COMM_WORLD.Get_rank(),
         num_shards=MPI.COMM_WORLD.Get_size(),
     )
@@ -53,24 +58,41 @@ def load_data(
         yield from loader
 
 
-def _list_image_files_recursively(data_dir):
+def _list_image_files_recursively(data_dir, txt=False):
     results = []
+    image_file_to_text_file = {}
     for entry in sorted(bf.listdir(data_dir)):
         full_path = bf.join(data_dir, entry)
         ext = entry.split(".")[-1]
         if "." in entry and ext.lower() in ["jpg", "jpeg", "png", "gif"]:
             results.append(full_path)
+            if txt:
+                prefix, _, ext = entry.rpartition(".")
+                path_txt = prefix + ".txt"
+                if bf.exists(path_txt):
+                    image_file_to_text_file[full_path] = path_txt
         elif bf.isdir(full_path):
-            results.extend(_list_image_files_recursively(full_path))
-    return results
+            next_results, next_map = _list_image_files_recursively(full_path)
+            results.extend(next_results)
+            image_file_to_text_file.update(next_map)
+    return results, image_file_to_text_file
 
 
 class ImageDataset(Dataset):
-    def __init__(self, resolution, image_paths, classes=None, shard=0, num_shards=1):
+    def __init__(self, resolution, image_paths,
+                 classes=None,
+                 image_file_to_text_file=None,
+                 txt=False,
+                 shard=0, num_shards=1):
         super().__init__()
         self.resolution = resolution
         self.local_images = image_paths[shard:][::num_shards]
         self.local_classes = None if classes is None else classes[shard:][::num_shards]
+        self.txt = txt
+
+        if self.txt:
+            self.local_images = [p for p in local_images if p in image_file_to_text_file]
+            self.local_texts = [image_file_to_text_file[p] for p in local_images]
 
     def __len__(self):
         return len(self.local_images)
@@ -103,4 +125,9 @@ class ImageDataset(Dataset):
         out_dict = {}
         if self.local_classes is not None:
             out_dict["y"] = np.array(self.local_classes[idx], dtype=np.int64)
+        if self.txt:
+            path_txt = self.local_texts[idx]
+            with bf.BlobFile(path_txt, "r") as f:
+                text = f.read()
+            out_dict['txt'] = text
         return np.transpose(arr, [2, 0, 1]), out_dict
