00fb7a3bdf3a69260e9d1579ee367ccea9b092a6
nostalgebraist <nostalgebraist@gmail.com>
2021-11-13 16:43:02 -0800

    args

--- improved_diffusion/script_util.py
+++ improved_diffusion/script_util.py
@@ -45,7 +45,7 @@ def model_and_diffusion_defaults():
         txt_depth=2,
         max_seq_len=64,
         txt_resolution=8,
-        cross_attn_channels_per_head=-1
+        cross_attn_channels_per_head=-1,
         verbose=False,
     )
 
