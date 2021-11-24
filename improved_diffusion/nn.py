53c6f814f42181a78c1187a63c21fe3d031f85aa
nostalgebraist <nostalgebraist@gmail.com>
2021-11-24 11:39:45 -0800

    whoops lol

--- improved_diffusion/nn.py
+++ improved_diffusion/nn.py
@@ -107,7 +107,7 @@ def normalization_1group(channels):
     :param channels: number of input channels.
     :return: an nn.Module for normalization.
     """
-    return GroupNorm32(32, channels)
+    return GroupNorm32(1, channels)
 
 
 def timestep_embedding(timesteps, dim, max_period=10000):
