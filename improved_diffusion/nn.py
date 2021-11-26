4cda8908317984f48554927e7b3939545caec880
nostalgebraist <nostalgebraist@gmail.com>
2021-11-26 11:29:21 -0800

    debug

--- improved_diffusion/nn.py
+++ improved_diffusion/nn.py
@@ -84,6 +84,7 @@ def update_ema(target_params, source_params, rate=0.99):
     :param source_params: the source parameter sequence.
     :param rate: the EMA rate (closer to 1 means slower).
     """
+    print((len(target_params), len(source_params)))
     for targ, src in zip(target_params, source_params):
         targ.detach().mul_(rate).add_(src, alpha=1 - rate)
 
