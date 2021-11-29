6e72e36a3291bfe6a58d9f2641cc7853c65b286a
nostalgebraist <nostalgebraist@gmail.com>
2021-11-29 15:31:35 -0800

    debug

--- improved_diffusion/text_nn.py
+++ improved_diffusion/text_nn.py
@@ -42,6 +42,8 @@ class LineEmbedding(nn.Module):
 
     def forward(self, x):
         n = (x == self.line_sep_id).to(torch.int).cumsum(dim=1).clamp(self.max_lines - 1)
+        print(x)
+        print(x == self.line_sep_id)
         print(n)
         pos_emb = self.emb(n)
         return pos_emb * self.scale
