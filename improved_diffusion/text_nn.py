fix th mha scaling

improved_diffusion/text_nn.py
@@ -192,9 +192,8 @@ class BetterMultiheadAttention(torch.nn.MultiheadAttention):
         self.k = torch.nn.Linear(src_embed_dim, src_embed_dim, bias=False)
         self.v = torch.nn.Linear(src_embed_dim, src_embed_dim, bias=False)
 
-        # using sqrt(head_dim) made things too equal, not scaling made things about right (?)
-        # self.scale = 1. # self.head_dim ** -0.5
-        self.scale = self.head_dim ** -0.5
+        # torch MHA scales down by sqrt(E) so we need to compensate
+        self.scale = self.num_heads ** 0.5
 
         self.register_parameter('in_proj_weight', None)
         self.register_parameter('in_proj_bias', None)