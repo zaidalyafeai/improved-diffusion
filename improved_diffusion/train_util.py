62f6906983f64d92148574185138cb7c153d3c91
nostalgebraist <nostalgebraist@gmail.com>
2021-12-02 13:07:19 -0800

    debug

--- improved_diffusion/train_util.py
+++ improved_diffusion/train_util.py
@@ -361,6 +361,7 @@ class TrainLoop:
         for p, name in zip(self.master_params, [*self.text_mods, *self.xattn_mods, 'xgain', 'other']):
             if p.grad is None:
                 continue
+            print(f"None grad for {name}")
             gn = np.sqrt((p.grad ** 2).sum().item())
             # nz = (p.grad == 0.).sum().item()
             if name in self.text_mods:
