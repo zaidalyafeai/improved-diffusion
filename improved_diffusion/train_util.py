684e0176aab41bfbdad19b800e87a24c2f07b16d
nostalgebraist <nostalgebraist@gmail.com>
2021-12-18 10:55:42 -0800

    fix

--- improved_diffusion/train_util.py
+++ improved_diffusion/train_util.py
@@ -273,7 +273,7 @@ class TrainLoop:
         ):
             batch, cond = next(self.data)
 
-            with torch.profiler.profile() as _p:
+            with th.profiler.profile() as _p:
                 self.run_step(batch, cond, verbose = (self.step % self.log_interval == 0))
             print(_p.key_averages().table(sort_by="self_cuda_time_total", row_limit=-1))
 
