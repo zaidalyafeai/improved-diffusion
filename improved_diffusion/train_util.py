0d8b1179a520e62e41916942ab2d782e3844a01e
nostalgebraist <nostalgebraist@gmail.com>
2022-01-21 10:07:03 -0800

    debug

--- improved_diffusion/train_util.py
+++ improved_diffusion/train_util.py
@@ -218,7 +218,8 @@ class TrainLoop:
                 self.ema_params = [
                     self._load_ema_parameters(rate) for rate in self.ema_rate
                 ]
-            except RuntimeError:
+            except RuntimeError as e:
+                raise e
                 print("couldn't load ema")
                 self.ema_params = [
                     copy.deepcopy(self.master_params) for _ in range(len(self.ema_rate))
