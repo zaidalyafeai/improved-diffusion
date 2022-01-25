fe007211a5ee5ef201929dbcb231cf9dd83f2ad6
nostalgebraist <nostalgebraist@gmail.com>
2022-01-24 17:47:18 -0800

    try mem efficient swish

--- improved_diffusion/nn.py
+++ improved_diffusion/nn.py
@@ -8,11 +8,33 @@ import torch as th
 import torch.nn as nn
 
 
-# PyTorch 1.7 has SiLU, but we support PyTorch 1.5.
-class SiLU(nn.Module):
-    def forward(self, x):
-        return x * th.sigmoid(x)
+# # PyTorch 1.7 has SiLU, but we support PyTorch 1.5.
+# class SiLU(nn.Module):
+#     def forward(self, x):
+#         return x * th.sigmoid(x)
 
+
+# from https://github.com/lukemelas/EfficientNet-PyTorch/blob/7e8b0d312162f335785fb5dcfa1df29a75a1783a/efficientnet_pytorch/utils.py
+# A memory-efficient implementation of Swish function
+class SwishImplementation(th.autograd.Function):
+    @staticmethod
+    def forward(ctx, i):
+        result = i * torch.sigmoid(i)
+        ctx.save_for_backward(i)
+        return result
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        i = ctx.saved_tensors[0]
+        sigmoid_i = th.sigmoid(i)
+        return grad_output * (sigmoid_i * (1 + i * (1 - sigmoid_i)))
+
+
+class MemoryEfficientSwish(nn.Module):
+    def forward(self, x):
+        return SwishImplementation.apply(x)
+
+SiLU = MemoryEfficientSwish
 
 class GroupNorm32(nn.GroupNorm):
     def forward(self, x):
