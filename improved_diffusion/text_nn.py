5de02d4933a1def986489707b729810a08bd08d8
nostalgebraist <nostalgebraist@gmail.com>
2022-01-27 13:04:36 -0800

    remove unneeded

--- improved_diffusion/text_nn.py
+++ improved_diffusion/text_nn.py
@@ -389,8 +389,8 @@ class CrossAttention(nn.Module):
                    'tmn', f"{max_over_src.min().item():.4f}",
                    ))
 
-        tgt_norm1 = (tgt.float() ** 2).sum().sqrt().item()
-        attn_norm = (attn_output.float() ** 2).sum().sqrt().item()
+            tgt_norm1 = (tgt.float() ** 2).sum().sqrt().item()
+            attn_norm = (attn_output.float() ** 2).sum().sqrt().item()
 
         if self.resid:
             tgt = tgt + attn_output
