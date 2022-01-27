7e398251d7e01b2c429a393f8c3e789e0af4c767
nostalgebraist <nostalgebraist@gmail.com>
2022-01-27 14:20:05 -0800

    work

--- improved_diffusion/unet.py
+++ improved_diffusion/unet.py
@@ -574,6 +574,7 @@ class UNetModel(nn.Module):
                 rotary_pos_emb=txt_rotary,
                 ff_glu=txt_ff_glu,
                 ff_mult=txt_ff_mult,
+                use_checkpoint=use_checkpoint
             )
 
         self.tgt_pos_embs = nn.ModuleDict({})
