c6a22e2dfdf9f64e8bd2d97f1839c890a1c59260
nostalgebraist <nostalgebraist@gmail.com>
2022-02-17 15:20:15 -0800

    whoops

--- improved_diffusion/crop.py
+++ improved_diffusion/crop.py
@@ -65,6 +65,7 @@ class RandomResizedProtectedCropLazy(torch.nn.Module):
                 print(f"safebox: {safebox}")
                 print(f"target_edgesize: {target_edgesize}")
                 print(f"protected_area: {protected_area}")
+                cropbox_left, cropbox_top, cropbox_right, cropbox_bottom = (0, 0, width, height)
 
         if return_n:
             return (cropbox_left, cropbox_top, cropbox_right, cropbox_bottom), n
