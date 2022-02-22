2dd3a075a29cf620a6ff24c5256fba4eea38dc20
nostalgebraist <nostalgebraist@gmail.com>
2022-02-22 11:51:24 -0800

    debug

--- improved_diffusion/crop.py
+++ improved_diffusion/crop.py
@@ -103,8 +103,8 @@ class RandomResizedProtectedCropLazy(torch.nn.Module):
 
         return (cropbox_left, cropbox_top, cropbox_right, cropbox_bottom)
 
-    def forward(self, img, safebox, debug=True):
-        cropbox = self.get_params(img, safebox, return_n=False, debug=debug)
+    def forward(self, img, safebox, px_scales=None, debug=True):
+        cropbox = self.get_params(img, safebox, px_scales, return_n=False, debug=debug)
         i, j = cropbox[1], cropbox[0]
         h, w = cropbox[2] - j, cropbox[3] - i
         # display(img.crop(cropbox).resize((self.size, self.size)))
