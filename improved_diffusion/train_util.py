f3df79cdd40fb425ec004ed01ce23d7ba10d92d1
nostalgebraist <nostalgebraist@gmail.com>
2022-03-11 09:17:48 -0800

    undebug

--- improved_diffusion/train_util.py
+++ improved_diffusion/train_util.py
@@ -607,7 +607,6 @@ class TrainLoop:
                              verbose=verbose)
 
     def _log_grad_norm(self):
-        return None  # testing
         sqsum = 0.0
         sqsum_text_encoder = 0.0
         has_text_encoder = False
