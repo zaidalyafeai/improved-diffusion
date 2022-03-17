064e3ab68443cf4c912d8eaca44f6cfe7cfbd7e0
nostalgebraist <nostalgebraist@gmail.com>
2022-03-17 11:28:05 -0700

    fix

--- scripts/super_res_sample.py
+++ scripts/super_res_sample.py
@@ -10,6 +10,7 @@ import blobfile as bf
 import numpy as np
 import torch as th
 import torch.distributed as dist
+import torchvision.transforms as T
 
 from improved_diffusion import dist_util, logger
 from improved_diffusion.script_util import (
