93625fddd8118052d12867820488add09d0b0fe3
nostalgebraist <nostalgebraist@gmail.com>
2022-03-17 17:37:31 -0700

    no one should need mpi part3

--- improved_diffusion/pipeline.py
+++ improved_diffusion/pipeline.py
@@ -87,7 +87,7 @@ class SamplingModel(nn.Module):
         plms_ddim_first_n=0,
         plms_ddim_last_n=None,
     ):
-        dist_util.setup_dist()
+        # dist_util.setup_dist()
 
         if self.is_super_res and low_res is None:
             raise ValueError("must pass low_res for super res")
