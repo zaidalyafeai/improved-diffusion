5b7145291587fdcdcd63933f10cbb25ba8247f18
nostalgebraist <nostalgebraist@gmail.com>
2022-03-29 09:40:12 -0700

    work

--- improved_diffusion/train_util.py
+++ improved_diffusion/train_util.py
@@ -498,7 +498,8 @@ class TrainLoop:
 
                 txt = th.as_tensor(tokenize(self.tokenizer, micro_cond['txt']), device=dist_util.dev())
                 capt = clip.tokenize(micro_cond['capt'], truncate=True).to(dist_util.dev())
-                micro_cond['txt'] = {'txt': txt, 'capt': capt}
+                micro_cond['txt'] = txt
+                micro_cond['capt'] = capt
             last_batch = (i + self.microbatch) >= batch.shape[0]
             t, weights = self.schedule_sampler.sample(micro.shape[0], dist_util.dev())
 
