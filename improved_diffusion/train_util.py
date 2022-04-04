514cdaf62d3d2cf6ba46dbe1ae7359a3a40268e8
nostalgebraist <nostalgebraist@gmail.com>
2022-04-04 12:12:33 -0700

    redo

--- improved_diffusion/train_util.py
+++ improved_diffusion/train_util.py
@@ -655,6 +655,9 @@ class TrainLoop:
             else:
                 pp = [p_]
 
+            if len(pp) == 0:
+                continue
+
             # vals = []
             gn = 0.
             for p in pp:
