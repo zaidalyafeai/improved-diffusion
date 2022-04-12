b33464e5f6a4c3fde7fca29731de206f15eb0596
nostalgebraist <nostalgebraist@gmail.com>
2022-04-11 20:47:18 -0700

    typo

--- improved_diffusion/image_datasets.py
+++ improved_diffusion/image_datasets.py
@@ -247,7 +247,7 @@ def load_data(
         avg_pkeep = np.mean([clip_pkeep(p) for p in clip_probs_by_idxs.values()])
         eff_len = avg_pkeep * len(dataset)
         eff_steps_per = eff_len / batch_size
-        print(f"avg_pkeep {avg_pkeep:.3f} | effective data size {eff_len:.1f} | effective steps/epohch {eff_steps_per:.1f}")
+        print(f"avg_pkeep {avg_pkeep:.3f} | effective data size {eff_len:.1f} | effective steps/epoch {eff_steps_per:.1f}")
     return _dataloader_gen(dataset, batch_size=batch_size, deterministic=deterministic, pin_memory=pin_memory,
                            prefetch_factor=prefetch_factor, clip_probs_by_idxs=clip_probs_by_idxs)
 
