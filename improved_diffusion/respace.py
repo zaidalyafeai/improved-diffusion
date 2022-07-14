868d02c838c9ebbe017163191ccc8ce508f47004
nostalgebraist <nostalgebraist@gmail.com>
2022-07-14 15:01:58 -0700

    fix

--- improved_diffusion/respace.py
+++ improved_diffusion/respace.py
@@ -143,3 +143,7 @@ class _WrappedModel:
         if self.rescale_timesteps:
             new_ts = new_ts.float() * (1000.0 / self.original_num_steps)
         return self.model(x, new_ts, **kwargs)
+
+    @property
+    def parameters(self):
+        return self.model.parameters
