c79f5ca70649254c195e0aed2254602ca14bc13b
nostalgebraist <nostalgebraist@gmail.com>
2022-08-07 11:31:08 -0700

    debug

--- improved_diffusion/unet.py
+++ improved_diffusion/unet.py
@@ -320,6 +320,8 @@ class ResBlock(TimestepBlock):
 
         self.fused = silu_impl=="fused"
 
+        print(f"ResBlock | channels={channels} base_channels={base_channels} out_channels={out_channels}")
+
         if base_channels == channels:
             base_channels = 0  # means "not expanded (in practice)"
 
