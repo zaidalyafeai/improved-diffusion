b1cadd18d5f26731ae7d05fb7edbaf32085ba02d
nostalgebraist <nostalgebraist@gmail.com>
2022-08-07 11:33:21 -0700

    fix

--- improved_diffusion/unet.py
+++ improved_diffusion/unet.py
@@ -1333,7 +1333,7 @@ class UNetModel(nn.Module):
         self.out = nn.Sequential(
             normalization(ch, base_channels=self.expand_timestep_base_dim, fused=silu_impl=="fused"),
             silu(impl=silu_impl, use_checkpoint=use_checkpoint_lowcost),
-            zero_module(conv_nd(dims, channel_mult[0] * model_channels, out_channels, 3, padding=1)),
+            zero_module(conv_nd(dims, int(channel_mult[0] * model_channels), out_channels, 3, padding=1)),
         )
 
         if monochrome_adapter:
